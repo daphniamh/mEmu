d21bdef3086a3e9602263293bc92e0f978ad8fb4
s0568272 <s0568272@htw-berlin.de>
2020-04-15 18:34:55 +0200

    design ready, memory arrays built, not working

--- app/src/main/java/com/example/memu/SecondFragment.java
+++ app/src/main/java/com/example/memu/SecondFragment.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import com.example.memu.R.drawable.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SecondFragment extends Fragment {
+
+
 
     @Override
     public View onCreateView(
@@ -18,16 +28,25 @@ public class SecondFragment extends Fragment {
     ) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
+
+
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+
+
+        view.findViewById(R.id.button_previous).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
+
+
+
+
+
             }
         });
     }

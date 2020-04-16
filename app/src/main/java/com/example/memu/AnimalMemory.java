0ef5b1d34b5a6435cc3cf1b93cd0188b89ff400a
s0568272 <s0568272@htw-berlin.de>
2020-04-16 17:04:21 +0200

    turning cards over and back working

--- app/src/main/java/com/example/memu/AnimalMemory.java
+++ app/src/main/java/com/example/memu/AnimalMemory.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.ArrayList;
@@ -20,7 +21,7 @@ public class AnimalMemory extends AppCompatActivity {
 
         //meine Programmcode-Beginn
 
-        ArrayList<Integer> images = new ArrayList<Integer>(7);
+        ArrayList<Integer> images = new ArrayList<Integer>(10);
         images.add(0, R.drawable.bat);
         images.add (1, R.drawable.bee);
         images.add (2, R.drawable.coala);
@@ -31,6 +32,16 @@ public class AnimalMemory extends AppCompatActivity {
         images.add (7, R.drawable.sheep);
         images.add (8, R.drawable.spider);
         images.add (9, R.drawable.turtle);
+        images.add(10, R.drawable.bat);
+        images.add (11, R.drawable.bee);
+        images.add (12, R.drawable.coala);
+        images.add (13, R.drawable.eagle);
+        images.add (14, R.drawable.elephant);
+        images.add (15, R.drawable.penguin);
+        images.add (16, R.drawable.shark);
+        images.add (17, R.drawable.sheep);
+        images.add (18, R.drawable.spider);
+        images.add (19, R.drawable.turtle);
 
         Button button1,button2, button3, button4, button5, button6, button7,
                 button8,button9, button10, button11, button12, button13, button14,
@@ -57,15 +68,48 @@ public class AnimalMemory extends AppCompatActivity {
         button19 = (Button) findViewById(R.id.button_ghost19);
         button20 = (Button) findViewById(R.id.button_ghost20);
 
-        Button[] button_list = {button1, button2, button3, button4, button5};
+        final Button[] button_list = {button1, button2, button3, button4, button5, button6,
+                button7, button8, button9, button10, button11, button12, button13, button14,
+                button15, button16, button17, button18, button19, button20};
+        final Integer[] images_list1 = {R.drawable.bat, R.drawable.coala, R.drawable.elephant,  //kann weg?
+                                 R.drawable.fox, R.drawable.turtle,
+                R.drawable.bat, R.drawable.coala, R.drawable.elephant,
+                R.drawable.fox, R.drawable.turtle };
 
-        Collections.shuffle(images);
+
+        Collections.shuffle(images); //Bilder werden geshuffelt
+        final Integer[] images_list = images.toArray(new Integer[images.size()]); //turn geshuffelte ArrayList into Array
+
+        final int[] counter = {0}; //final Array, damit diese Variable in der Click-Methode nutzbar ist, Workaround vorschlag von Android Stuiod
+            //genaueers hier: https://stackoverflow.com/questions/14425826/variable-is-accessed-within-inner-class-needs-to-be-declared-final
+        //um zu überprüfen, wann Vorder und Rückseite der Karten zu sehen sind, im Tutorial nimmt man unsichtbaren Buttontext
 
         for(int i=0;i<button_list.length;i++){
-            button_list[i].setBackgroundResource(R.drawable.bat);
+            //button_list[i].setBackgroundResource(R.drawable.bat);
+            //button_list[i].text = "cardBack";
+            //button_list[i].setText = "cardBack";
+            final int finalI = i;
+            final int finalI1 = i;
+            button_list[i].setOnClickListener(new View.OnClickListener() { //beim Anklicken eines Buttons, wird ein Bild aus der Liste dahinter eingefügt
+                @Override
+                public void onClick(View v) {
+
+                    if(counter[0] == 0) {
+                        button_list[finalI].setBackgroundResource(images_list[finalI]);
+                        counter[0] = 1;
+                    }
+                    else{
+                        button_list[finalI].setBackgroundResource(R.drawable.ghost);
+                        counter[0] =0;
+                    }
+
+                    }
+            }); //diese Klammer + ; ist wichtig!
+
         }
 
     //VideoMinute 23.48 min
+        //https://www.youtube.com/watch?v=BGvjScKcW1s
 
 
         //ArrayList<Button> buttons   =new ArrayList<Button>(20);
